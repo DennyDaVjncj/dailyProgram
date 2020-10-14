a85265e1de2892eb6fc8b98ac4e6dde1c14c418b
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-14 17:26:40 -0400

    psuudoCode/new function

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 let now=moment();//fulfilled requirement
 let nowDisplayed=moment().format('dddd');
+let thisMoment=$('#currentDay');
 
-//target empty paragraph element in jumbotron for moment.js use
-let p=$('#currentDay');
-p.text(nowDisplayed)
+//quantifying the present moment
+function presentMoment(){
+    thisMoment.text(nowDisplayed);
+}presentMoment();//quick on the trigger
+
+//program dynamic time blocks that can take input
+    //color code time blocks to represent past, present, future tasks
+//current hour displayed on side of block, another icon maybe for the other side
+    //each block populates with same theme
+
+//master <div> with styling/dynamo attached
+    //gets reproduced winthin .each() upon click of element tagged by #id,.class or whatever
+
+function dynamicDivs(nowBlock){
+    let nowBlock=$("#dynamicMutation");//div element, clientSide
+    
+}
