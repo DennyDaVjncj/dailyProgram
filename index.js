845219ec43e07e06c96be3d5488ed65e60c2b808
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-17 16:38:13 -0400

    complete overhaul

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //global options
 let now=moment();
-let thisMoment=$('#currentDay');
+console.log(now);
+let thisMoment=$('#currentDay');//displayes real time
+let activityBlock=$(".time-block");//html target to add tenses
+let txtArea=$("textarea");//<textarea>, border
+let hourBlock
 
+//handles moment.js functionality
 function presentMoment(){
     thisMoment.text(now);
 }presentMoment();//quick on the trigger
@@ -12,15 +17,19 @@ function presentMoment(){
 //if statement is checking that <textarea> has value present
 //we have to check for occupying value compared with current time
 //.each() timeBlock #id will be stored within array, to be indexed through
+    //each #id representing every hour in workday, gets compared to current time, establishing tense
+//on click of button on far right, <textarea> input needs to be stored to localStorage
 
-
-let DOM=$(".time-block");//html target to add tenses
-let txtArea=$("textarea");//<textarea>, border
-function everyTense(){
-    if(txtArea.val().trim().length<1){
-        alert("succesful check on user input");
-    }
+//logic for dynamic tenses - set tenses right now
+//.hour is PRIMARY TARGET
+//function for each #id, each time block 
+function everyTense(){    
+    activityBlock;    
 }everyTense();
+
+// function everyTense(){
+//     if(now)
+// }
 //complete functionality before 12am!
 
 
